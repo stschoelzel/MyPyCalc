e305e68b21f67172340caf0f4e58a4706f66bfdd
stschoelzel <info@stschoelzel.de>
2024-09-17 18:25:46 +0200

    Setup

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+def main():
+    # Hauptlogik deines Programms
+    print("Programm wird ausgeführt")
+
+if __name__ == '__main__':
+    main()
+
+window = Tk()
+window.title("MyPyCalc")
+window.geometry("600x400")
+
